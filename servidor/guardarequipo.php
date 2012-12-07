a8784be77dfe141534fb7990ce86cb13d1d3441c
Evijair Nuñez <nujev2008@gmail.com>
2012-12-06 19:54:05 -0600

    archivos para el servidor

---
+++ servidor/guardarequipo.php
@@ -0,0 +1,18 @@
+<?php
+
+$connection= mysql_connect("localhost","root","ok...") or die("Connection Failed".mysql_error());
+mysql_select_db("proyectorfid",$connection)or die("Error loading the DataBase".mysql_error());
+
+$tag = $_POST['tagrfid'];
+$nombre =  $_POST['nombre'];
+$base =  $_POST['base'];
+$email =  $_POST['email'];
+
+$query = "INSERT INTO persona(tagrfid, name,email, base, regla) 
+		  VALUES ('$tag','$nombre','$email','$base', 0)";
+
+$result = mysql_query($query); 
+header('location: ../index.html');
+echo mysql_error();
+
+?>
